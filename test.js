97f5a617a31b0190c94d727f9a3c1ad576e29f6c
haneen habash <haneenhabash08.com>
2026-02-09 16:54:02 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  if (b === 0) return "Cannot divide by zero";
+  return a / b;
+}
+
+// Test runs
+console.log("Add:", add(5, 3));
+console.log("Sub:", sub(5, 3));
+console.log("Mul:", mul(5, 3));
+console.log("Div:", div(5, 3));
