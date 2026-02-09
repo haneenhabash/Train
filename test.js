0833b4cd2b7630a5d4812a58dc1f587487961a5c
haneen habash <haneenhabash08.com>
2026-02-09 22:31:47 +0200

    edit

--- test.js
+++ test.js
@@ -14,8 +14,8 @@ function power2(a, b) {
   if (b === 0) return "Cannot divide by zero";
   return a / b;
 }
-function mod(a, b) {
-  return a % b;
+function mod3333(a, b) {
+  return a % b %c;
 }
 
 console.log("Mod:", mod(10, 3));
@@ -28,6 +28,6 @@ function div(a, b) {
 
 // Test runs
 console.log("Add:", add(5, 3));
-console.log("Sub:", sub(5, 3));
+console.log("Sub222:", sub(5, 3));
 console.log("Mul:", mul(5, 3));
 console.log("Div:", div(5, 3));
